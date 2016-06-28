52ba41f049821d7bbde06e4860c7af63e9007e63
Ekaterina Kashchenko <katikate123@gmail.com>
2016-06-28 10:14:40 +0300

    trying to get data from api

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,23 @@
-$(function() {
-	console.log('quote');
-});
+$(function(){
+		$('.getNewQuote').click(function(){ 
+			console.log('click');
+				var quote = {
+					getQuote : function () {
+			 			var getQuoteCategories = {
+			 				type: 'get',
+			 				url: 'http://quotes.rest/qod.json',
+			 				dataType: 'json',
+			 				success: function(data) {
+			 					console.log('Pulled from API!!');
+			 					alert(data);
+			 		    },
+			 			   error: function() {
+			 			    console.log('Something did not happen as intended');
+			 			   }
+			 			};
+					}
+				};
+				quote.getQuote();
+		});
+
+	}); // end of document.ready
