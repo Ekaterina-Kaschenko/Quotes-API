f49a7700378029132037a7d3a08764306fde6ce0
Ekaterina Kashchenko <katikate123@gmail.com>
2016-06-28 14:28:17 +0300

    received response from api

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,10 +3,17 @@ $(function(){
 			console.log('click');
 				var quote = {
 					getQuote : function () {
-			 			var getQuoteCategories = {
-			 				type: 'get',
-			 				url: 'http://quotes.rest/qod.json',
-			 				dataType: 'json',
+						var apiKey = 'eLDaLcXAYOmshU8uqTBkhkrT9Nmcp1ACCmSjsnmbo1Va9oqckY';
+						
+			 			$.ajax({
+			 				//type: 'get',
+							headers: {
+			          'X-Mashape-Key': 'eLDaLcXAYOmshU8uqTBkhkrT9Nmcp1ACCmSjsnmbo1Va9oqckY',
+			          'Content-Type': 'application/x-www-form-urlencoded',
+			          'Accept': 'application/json'
+			        },
+			 				url: 'https://andruxnet-random-famous-quotes.p.mashape.com/',
+			 				// dataType: 'json',
 			 				success: function(data) {
 			 					console.log('Pulled from API!!');
 			 					alert(data);
@@ -14,10 +21,9 @@ $(function(){
 			 			   error: function() {
 			 			    console.log('Something did not happen as intended');
 			 			   }
-			 			};
+			 			});
 					}
 				};
-				quote.getQuote();
+			quote.getQuote();
 		});
-
 	}); // end of document.ready
