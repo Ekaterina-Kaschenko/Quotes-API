e4e1a60ebbec7d37e6574588d7a6174b26cb6187
Ekaterina Kashchenko <katikate123@gmail.com>
2016-06-29 00:51:26 +0300

    made task only on jquery

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,36 +1,34 @@
 $(function(){
 	function getQuote() {
 		$.ajax({
-			//type: 'get',
+			type: 'get',
 		headers: {
       'X-Mashape-Key': 'eLDaLcXAYOmshU8uqTBkhkrT9Nmcp1ACCmSjsnmbo1Va9oqckY',
       'Content-Type': 'application/x-www-form-urlencoded',
       'Accept': 'application/json'
     },
 			url: 'https://andruxnet-random-famous-quotes.p.mashape.com/',
-			// dataType: 'json',
 			success: function(data) {
 				console.log(data);
-				var wrapperObj = document.getElementsByClassName('wrapper')[0],
-						getNewQuote = document.getElementsByClassName('getNewQuote')[0],
-						socialButton = document.getElementsByClassName('social-buttons__item');
+				var wrapperObj = $('.wrapper')[0],
+						getNewQuote = $('.getNewQuote')[0],
+						socialButton = $('.social-buttons__item');
 
 				var dataObj = JSON.parse(data);
-				var quoteContainer = document.getElementsByClassName('quoteContainer')[0];
-					quoteContainer.innerHTML = dataObj.quote;
-				var authorContainer = document.getElementsByClassName('authorContainer')[0];
-					authorContainer.innerHTML = dataObj.author;
-					var randomColor = getRandomColor();
-					wrapperObj.style.backgroundColor = randomColor;
-					getNewQuote.style.backgroundColor = randomColor;
-					var socialButtonsLength = $('.social-buttons__item').length; 
+				var quoteContainer = $('.quoteContainer')[0];
+					$(quoteContainer).html(dataObj.quote);
+				var authorContainer = $('.authorContainer')[0];
+					$(authorContainer).html(dataObj.author);
+				var randomColor = getRandomColor();
+					$(wrapperObj).css('background-color', randomColor);
+					$(getNewQuote).css('background-color', randomColor);
+				var socialButtonsLength = $('.social-buttons__item').length; 
 					
 					for (var i = 0; i < socialButtonsLength; i++) {
-						socialButton[i].style.backgroundColor = randomColor;
+						$(socialButton[i]).css('background-color', randomColor);
 					}
-					
-					quoteContainer.style.color = randomColor;
-					authorContainer.style.color = randomColor;
+					$(quoteContainer).css('color', randomColor);
+					$(authorContainer).css('color', randomColor);
 	    },
 		   error: function() {
 		    console.log('Something did not happen as intended');
@@ -46,8 +44,7 @@ $(function(){
 	  return 'rgb(' + getRandomNum(255) + ',' + getRandomNum(255) + ',' + getRandomNum(255) + ')';
 	}
 
-	//Случайное целое число в диапазоне (0, max]
 	function getRandomNum(max) {
 	  return Math.round ( Math.random() * max );
 	}
-}); // end of document.ready
+}); 
