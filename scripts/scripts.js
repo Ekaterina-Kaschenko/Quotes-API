7019938daa74c785c804825adbe95c37dad7a4e9
Ekaterina Kashchenko <katikate123@gmail.com>
2016-06-29 00:13:10 +0300

    made separate function to load all data after loading page

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,60 +1,53 @@
 $(function(){
-		$('.getNewQuote').click(function(){ 
-			console.log('click');
-				var quote = {
-					getQuote : function () {
-						var apiKey = 'eLDaLcXAYOmshU8uqTBkhkrT9Nmcp1ACCmSjsnmbo1Va9oqckY';
-						
-			 			$.ajax({
-			 				//type: 'get',
-							headers: {
-			          'X-Mashape-Key': 'eLDaLcXAYOmshU8uqTBkhkrT9Nmcp1ACCmSjsnmbo1Va9oqckY',
-			          'Content-Type': 'application/x-www-form-urlencoded',
-			          'Accept': 'application/json'
-			        },
-			 				url: 'https://andruxnet-random-famous-quotes.p.mashape.com/',
-			 				// dataType: 'json',
-			 				success: function(data) {
-			 					console.log(data);
-			 					var wrapperObj = document.getElementsByClassName('wrapper')[0],
-			 							getNewQuote = document.getElementsByClassName('getNewQuote')[0],
-			 							socialButton = document.getElementsByClassName('social-buttons__item');
-			 							console.log(socialButton);
+	function getQuote() {
+		$.ajax({
+			//type: 'get',
+		headers: {
+      'X-Mashape-Key': 'eLDaLcXAYOmshU8uqTBkhkrT9Nmcp1ACCmSjsnmbo1Va9oqckY',
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Accept': 'application/json'
+    },
+			url: 'https://andruxnet-random-famous-quotes.p.mashape.com/',
+			// dataType: 'json',
+			success: function(data) {
+				console.log(data);
+				var wrapperObj = document.getElementsByClassName('wrapper')[0],
+						getNewQuote = document.getElementsByClassName('getNewQuote')[0],
+						socialButton = document.getElementsByClassName('social-buttons__item');
 
-			 					var dataObj = JSON.parse(data);
-			 					console.log(dataObj.quote);
-			 					var quoteContainer = document.getElementsByClassName('quoteContainer')[0];
-			 						quoteContainer.innerHTML = dataObj.quote;
-			 					var authorContainer = document.getElementsByClassName('authorContainer')[0];
-			 						authorContainer.innerHTML = dataObj.author;
-			 						var randomColor = getRandomColor();
-			 						wrapperObj.style.backgroundColor = randomColor;
-			 						getNewQuote.style.backgroundColor = randomColor;
-			 						var socialButtonsLength = $('.social-buttons__item').length; 
-			 						
-			 						for (var i = 0; i < socialButtonsLength; i++) {
-			 							socialButton[i].style.backgroundColor = randomColor;
-			 							console.log('ki');
-			 						}
-			 						
-			 						quoteContainer.style.color = randomColor;
-			 						authorContainer.style.color = randomColor;
-			 		    },
-			 			   error: function() {
-			 			    console.log('Something did not happen as intended');
-			 			   }
-			 			});
+				var dataObj = JSON.parse(data);
+				var quoteContainer = document.getElementsByClassName('quoteContainer')[0];
+					quoteContainer.innerHTML = dataObj.quote;
+				var authorContainer = document.getElementsByClassName('authorContainer')[0];
+					authorContainer.innerHTML = dataObj.author;
+					var randomColor = getRandomColor();
+					wrapperObj.style.backgroundColor = randomColor;
+					getNewQuote.style.backgroundColor = randomColor;
+					var socialButtonsLength = $('.social-buttons__item').length; 
+					
+					for (var i = 0; i < socialButtonsLength; i++) {
+						socialButton[i].style.backgroundColor = randomColor;
 					}
-				};
-			quote.getQuote();
+					
+					quoteContainer.style.color = randomColor;
+					authorContainer.style.color = randomColor;
+	    },
+		   error: function() {
+		    console.log('Something did not happen as intended');
+		   }
 		});
+	}
+	getQuote();
+	$('.getNewQuote').click(function(){ 
+		getQuote(); 
+	});
 
-		function getRandomColor() {
-		    var letters = '0123456789ABCDEF'.split('');
-		    var color = '#';
-		    for (var i = 0; i < 6; i++ ) {
-		        color += letters[Math.floor(Math.random() * 16)];
-		    }
-		    return color;
-		}
-	}); // end of document.ready
+	function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+	}
+}); // end of document.ready
