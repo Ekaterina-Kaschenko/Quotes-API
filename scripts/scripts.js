4d3d0f3086cb419f981efb5f2f4fbc83e2b5b7d0
Ekaterina Kashchenko <katikate123@gmail.com>
2016-06-30 23:28:08 +0300

    vk share button done

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -32,10 +32,47 @@ $(function(){
 					$(quoteContainer).css('color', randomColor);
 					$(authorContainer).css('color', randomColor);
 				
+					var Share = {
+						vkontakte: function(purl, ptitle, pimg, text) {
+							url  = 'http://vkontakte.ru/share.php?';
+							url += 'url='          + encodeURIComponent(purl);
+							url += '&title='       + encodeURIComponent('"' + currentQuote + '".');
+							url += '&description=' + encodeURIComponent(text);
+							url += '&image='       + encodeURIComponent(pimg);
+							url += '&noparse=true';
+							Share.popup(url);
+						},
+
+						facebook: function(purl, ptitle, pimg, text) {
+							url  = 'http://www.facebook.com/sharer.php?s=100';
+							url += '&p[title]='     + encodeURIComponent(ptitle);
+							url += '&p[summary]='   + encodeURIComponent(text);
+							url += '&p[url]='       + encodeURIComponent(purl);
+							url += '&p[images][0]=' + encodeURIComponent(pimg);
+							Share.popup(url);
+						},
+						twitter: function(purl, ptitle) {
+							url  = 'http://twitter.com/share?';
+							url += 'text='      + encodeURIComponent(ptitle);
+							url += '&url='      + encodeURIComponent(purl);
+							url += '&counturl=' + encodeURIComponent(purl);
+							Share.popup(url);
+						},
+
+						popup: function(url) {
+							window.open(url);
+						}
+					};
+
+					
+					$('.vk').click(function() {
+						Share.vkontakte('URL','TITLE','IMG_PATH', currentAuthor);
+					});
+					$('.facebook').click(function() {
+						Share.facebook('URL','TITLE','IMG_PATH','DESC');
+					});
 					$('.twitter').click(function() {
-				    if(!inIframe()) {
-				      openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
-				    }
+						Share.twitter('URL','TITLE');
 					});
 
 	    },
