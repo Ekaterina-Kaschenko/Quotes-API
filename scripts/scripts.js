3ba2500c6f526b437cfc4b1206d5c174a77f6b35
Ekaterina Kashchenko <katikate123@gmail.com>
2016-07-05 12:38:59 +0300

    fixed the structure of the application; did everything using protopypes; vk and twitter buttond are working

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,107 +1,118 @@
-$(function(){
-	function getQuote() {
-		$.ajax({
-			type: 'get',
-		headers: {
-      'X-Mashape-Key': 'eLDaLcXAYOmshU8uqTBkhkrT9Nmcp1ACCmSjsnmbo1Va9oqckY',
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Accept': 'application/json'
-    },
-			url: 'https://andruxnet-random-famous-quotes.p.mashape.com/',
-			success: function(data) {
-				//get data
-				console.log(data);
+'use strict'
+$(function() {
+  function Quotes() {
+    var self = this;
+    self.wrapperObj = $('.wrapper');
+    self.newQuoteButton = $('.getNewQuote');
+    self.socialButton = $('.social-buttons__item');
+    self.quoteContainer = $('.quoteContainer');
+    self.authorContainer = $('.authorContainer');
+    
+    self.dataObj = null;
+    self.updateQuote();
+    $('.vk').click(function() {
+      self.shareVK('URL', 'TITLE', 'IMG_PATH', self.getQuoteAuthor());
+    });
+    $('.twitter').click(function() {
+      self.shareTwitter('URL', '"' + self.getQuoteAuthor() + '" '  + self.getQuoteText);
+    });
+    $('.facebook').click(function() {
+      self.shareFacebook('URL', 'TITLE', 'IMG_PATH', self.getQuoteAuthor());
+    });
+    
+    return self;
+  }
 
-				// create dom
-				function createDOM () {
-					var wrapperObj = $('.wrapper')[0],
-						getNewQuote = $('.getNewQuote')[0],
-						socialButton = $('.social-buttons__item');
+  Quotes.prototype.updateQuote = function() {
+  	var self = this;
+    $.ajax({
+      type: 'get',
+      headers: {
+        'X-Mashape-Key': 'eLDaLcXAYOmshU8uqTBkhkrT9Nmcp1ACCmSjsnmbo1Va9oqckY',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept': 'application/json'
+      },
+      url: 'https://andruxnet-random-famous-quotes.p.mashape.com/',
+      success: function(data) {
+        console.log(data);
+        self.dataObj = JSON.parse(data);
+        self.updateContent();
+      },
+      error: function() {
+        console.log('Something did not happen as intended');
+      }
+    });
+  };
+  Quotes.prototype.updateContent = function() {
+  	var self = this;
+    self.quoteContainer.html(self.getQuoteText());
+    self.authorContainer.html(self.getQuoteAuthor());
+    
+    var randomColor = getRandomColor();
+    self.wrapperObj.css('background-color', randomColor);
+    self.newQuoteButton.css('background-color', randomColor);
+    for (var i = 0; i < self.socialButton.length; i++) {
+      self.socialButton[i].style['background-color'] = randomColor;
+    }
+    return self;
+  };
+  Quotes.prototype.shareVK = function(purl, ptitle, pimg, text) {
+  	var self = this;
+    var url = 'http://vkontakte.ru/share.php?';
+    url += 'url=' + encodeURIComponent(purl);
+    url += '&title=' + encodeURIComponent('"' + self.getQuoteText() + '" ' +
+    	self.getQuoteAuthor());
+    url += '&description=' + encodeURIComponent(text);
+    url += '&image=' + encodeURIComponent(pimg);
+    url += '&noparse=true';
+    console.log(url);
+    window.open(url);
+    return self;
+  };
+  Quotes.prototype.shareFacebook =  function(purl, ptitle, pimg, text) {
+    var self = this;
+    var url  = 'http://www.facebook.com/sharer.php?s=100';
+    url += '&p[title]='     + encodeURIComponent('"' + self.getQuoteText() + '" ' +
+      self.getQuoteAuthor());
+    url += '&p[summary]='   + encodeURIComponent(text);
+    url += '&p[url]='       + encodeURIComponent(purl);
+    url += '&p[images][0]=' + encodeURIComponent(pimg);
+    console.log(url);
+    window.open(url);
+    return self;
+  };
+  Quotes.prototype.shareTwitter = function(purl, ptitle) {
+    var self = this;
+    var url  = 'http://twitter.com/share?';
+    url += 'text='      + encodeURIComponent('"' + self.getQuoteText() + '" ' +
+      self.getQuoteAuthor());
+    url += '&url='      + encodeURIComponent(purl);
+    url += '&counturl=' + encodeURIComponent(purl);
+    console.log(url);
+    window.open(url);
+    return self;
+  };
 
-					var dataObj = JSON.parse(data);
-					var currentQuote = dataObj.quote;
-					var currentAuthor = dataObj.author;
-					var quoteContainer = $('.quoteContainer')[0];
-						$(quoteContainer).html(currentQuote);
-					var authorContainer = $('.authorContainer')[0];
-						$(authorContainer).html(currentAuthor);
-					var randomColor = getRandomColor();
-						$(wrapperObj).css('background-color', randomColor);
-						$(getNewQuote).css('background-color', randomColor);
-					var socialButtonsLength = $('.social-buttons__item').length; 
-
-						for (var i = 0; i < socialButtonsLength; i++) {
-							$(socialButton[i]).css('background-color', randomColor);
-						}
-
-						$(quoteContainer).css('color', randomColor);
-						$(authorContainer).css('color', randomColor);
-				}
-
-
-					var Share = {
-						vkontakte: function(purl, ptitle, pimg, text) {
-							url  = 'http://vkontakte.ru/share.php?';
-							url += 'url='          + encodeURIComponent(purl);
-							url += '&title='       + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor);
-							url += '&description=' + encodeURIComponent(text);
-							url += '&image='       + encodeURIComponent(pimg);
-							url += '&noparse=true';
-							Share.popup(url);
-						},
-
-						facebook: function(purl, ptitle, pimg, text) {
-							url  = 'http://www.facebook.com/sharer.php?s=100';
-							url += '&p[title]='     + encodeURIComponent(currentAuthor);
-							url += '&p[summary]='   + encodeURIComponent(text);
-							url += '&p[url]='       + encodeURIComponent(currentAuthor);
-							url += '&p[images][0]=' + encodeURIComponent(pimg);
-							Share.popup(url + 'jjjjj');
-						},
-
-						twitter: function(purl, ptitle) {
-							url  = 'http://twitter.com/share?';
-							url += 'text='      + encodeURIComponent(ptitle);
-							url += '&url='      + encodeURIComponent(purl);
-							url += '&counturl=' + encodeURIComponent(purl);
-							Share.popup(url);
-						},
-
-						popup: function(url) {
-							window.open(url);
-						}						
-						
-					};
-
-					$('.vk').click(function() {
-						Share.vkontakte('URL','TITLE','IMG_PATH', currentAuthor);
-					});
-					$('.twitter').click(function() {
-						Share.twitter('URL', '"' + currentQuote + '" '  + currentAuthor);
-					});
-	    },
-		   error: function() {
-		    console.log('Something did not happen as intended');
-		   }
-		});
-	}
+  Quotes.prototype.getQuoteText = function() {
+  	return this.dataObj.quote;
+  };
+  Quotes.prototype.getQuoteAuthor = function() {
+  	return this.dataObj.author;
+  };
 	
-	getQuote();
+  var quotes = new Quotes();
 
-	$('.getNewQuote').click(function(){ 
-		getQuote(); 
-	});
+  $('.getNewQuote').click(function() {
+    quotes.updateQuote();
+  });
 
-	function getRandomColor() {
-		var colors = ['#7d4627', '#89bdd3', '#6ed3cf', '#9068be', '#e62739', '#3fb0ac','#173e43', '#312c32', '#5a5c51', '#729f98', '#283018', '#aa863a', '#6534ff', '#62bcfa', '#5e0231', '#856046', '#e05038', '#e6af4b', '#300032', '#c43235', '#16174f', '#963019', '', '#e05915'];
-	  var color = colors[getRandomNum(colors.length - 1)];
+});
 
-	  return color;
-	}
+function getRandomColor() {
+    var colors = ['#7d4627', '#89bdd3', '#6ed3cf', '#9068be', '#e62739', '#3fb0ac', '#173e43', '#312c32', '#5a5c51', '#729f98', '#283018', '#aa863a', '#6534ff', '#62bcfa', '#5e0231', '#856046', '#e05038', '#e6af4b', '#300032', '#c43235', '#16174f', '#963019', '', '#e05915'];
+    return colors[getRandomNum(colors.length - 1)];
+  }
 
-	function getRandomNum(max) {
-	  return Math.round ( Math.random() * max );
-	}
-
-
-}); 
+  function getRandomNum(max) {
+    return Math.round(Math.random() * max);
+  }
